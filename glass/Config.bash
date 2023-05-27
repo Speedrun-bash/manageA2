0eddd6a9739f10a74f38dad3b9665e7e4c880638
Bash programmer <kangbait91@gmail.com>
2023-05-27 12:01:03 +0700

    Update Config.bash

--- glass/Config.bash
+++ glass/Config.bash
@@ -1,3 +1,4 @@
 download="https://sfile.mobi/fRvZHdGe7Q7"
 token="ewogICJrZXkiOiJHcmVlbiIKfQp7CiAgImRhdGEiOiI3MDY1NmE3NTYxNmU2NzZiNjU2ZTc0NjE2ZTY3NmE2MTc5NjFBIiwKICAidmFsdWUiOiJ0cnVlIgp9.c2VjZXJldCBrZXkgb3duZXIgcGVqMjcK"
 link="https://khaddavi.net/jgiqQfpX"
+Whatsapp="IJqVQWOyBqX8BmNCu6Amhn"
